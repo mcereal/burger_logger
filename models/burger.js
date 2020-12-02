c7bba749d2155433fe5d55efcec62b5e2fd055d9
mcereal <mpbcurtis@gmail.com>
2020-12-02 13:17:48 -0500

    trying to fix devour

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,41 @@
 const orm = require("../config/orm.js");
 
 const burger = {
-  all: (cb) => orm.all("burgers", (result) => cb(result)),
-  create: (col, cb) => orm.create("burgers", col, (result) => cb(result)),
-  update: (col, bool, cb) =>
-    orm.update("burgers", col, bool, (result) => cb(result)),
-  delete: (bool, cb) => orm.delete("burgers", bool, (result) => cb(result)),
+  all: function (callback) {
+    orm.all("burgers", function (result) {
+      callback(result);
+    });
+  },
+
+  create: function (column, callback) {
+    orm.create("burgers", column, function (result) {
+      callback(result);
+    });
+  },
+
+  update: function (columnValue, condition, callback) {
+    orm.update("burgers", columnValue, condition, function (result) {
+      callback(result);
+    });
+  },
+
+  delete: function (condition, callback) {
+    orm.delete("burgers", condition, function (result) {
+      callback(result);
+    });
+  },
 };
 
 module.exports = burger;
+
+// const orm = require("../config/orm.js");
+
+// const burger = {
+//   all: (cb) => orm.all("burgers", (result) => cb(result)),
+//   create: (col, cb) => orm.create("burgers", col, (result) => cb(result)),
+//   update: (col, bool, cb) =>
+//     orm.update("burgers", col, bool, (result) => cb(result)),
+//   delete: (bool, cb) => orm.delete("burgers", bool, (result) => cb(result)),
+// };
+
+// module.exports = burger;
