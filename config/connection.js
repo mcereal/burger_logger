94acf3de5539b747cd05c61c4e8f3aacc38fc2a3
mcereal <mpbcurtis@gmail.com>
2020-12-02 16:25:47 -0500

    jawsdb again

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,5 @@
 const mysql = require("mysql");
 let connection;
-const JAWSDB_URL =
-  "mysql://rdw46xmkzobzguaj:kpqk9s5p0yl4x3wi@de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/md2r7rektosrt1bb";
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
