0e8df0179c9e749f0846f9063dd47a1f59bb3516
mcereal <mpbcurtis@gmail.com>
2020-12-02 10:07:10 -0500

    schema and seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+  
+USE burgers_db;
+
+INSERT INTO burgers (burger_type)
+VALUES ("hamburger");
+
+INSERT INTO burgers (burger_type)
+VALUES ("cheeseburger");
+
+INSERT INTO burgers (burger_type)
+VALUES ("veggieburger");
